1fe1dededdd3ddfcefa5cfcf47c33dc1756b8ec2
Chenyu Xi <xi_chenyu@163.com>
2019-04-22 14:41:28 -0400

    add simple GRU model

---
+++ scripts/gru_model.py
@@ -0,0 +1,73 @@
+import torch.nn as nn
+import torch
+torch.manual_seed(1)
+
+
+class GruModel(nn.Module):
+    def __init__(self):
+        super(GruModel, self).__init__()
+        cov1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=1)
+        torch.nn.init.xavier_uniform_(cov1.weight)
+        self.convBlock1 = nn.Sequential(cov1,
+                                        nn.BatchNorm2d(64),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(kernel_size=2))
+
+        cov2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        torch.nn.init.xavier_uniform_(cov2.weight)
+        self.convBlock2 = nn.Sequential(cov2,
+                                        nn.BatchNorm2d(128),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(kernel_size=2))
+
+        cov3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        torch.nn.init.xavier_uniform_(cov3.weight)
+        self.convBlock3 = nn.Sequential(cov3,
+                                        nn.BatchNorm2d(256),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(kernel_size=4))
+
+        cov4 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1)
+        torch.nn.init.xavier_uniform_(cov4.weight)
+        self.convBlock4 = nn.Sequential(cov4,
+                                        nn.BatchNorm2d(512),
+                                        nn.ReLU(),
+                                        nn.MaxPool2d(kernel_size=4))
+
+        self.fcBlock1 = nn.Sequential(nn.Linear(in_features=2048, out_features=1024),
+                                      nn.ReLU(),
+                                      nn.Dropout(0.5))
+
+        self.GruBlock1 = nn.Sequential(nn.GRU(input_size=1024,
+                                              hidden_size=256,
+                                              num_layers=2,
+                                              batch_first=True,
+                                              bidirectional=True),
+                                       nn.BatchNorm1d(256),
+                                       nn.Dropout(0.5))
+
+        self.output = nn.Sequential(nn.Linear(in_features=256, out_features=10),
+                                    nn.Softmax(dim=1))
+
+    def forward(self, inp):
+
+        out = self.convBlock1(inp)
+        out = self.convBlock2(out)
+        out = self.convBlock3(out)
+        out = self.convBlock4(out)
+
+        out = out.view(out.size()[0], -1)
+        out = self.fcBlock1(out)
+        out = self.GruBlock1(out)
+        # out_features=256
+
+        out = self.output(out)
+        return out
+
+
+G_model = GruModel()
+
+if __name__ == '__main__':
+    print(G_model)
+
+
