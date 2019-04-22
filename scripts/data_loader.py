a3b4bacafe7b0339d2174f191bfe146eab9dac65
Chenyu Xi <xi_chenyu@163.com>
2019-04-22 14:54:31 -0400

    fix lose

---
+++ scripts/data_loader.py
@@ -0,0 +1,56 @@
+import torch
+import numpy as np
+import h5py
+from torch.utils.data import Dataset, DataLoader
+from Paras import Para
+
+
+class TorchData(Dataset):
+
+    def __init__(self, dataset_path):
+        """
+        Take the h5py dataset
+        """
+        super(TorchData, self).__init__()
+        self.dataset = h5py.File(dataset_path, 'r')
+        self.mel = self.dataset['mel']
+        self.tag = self.dataset['tag']
+
+        self.len = self.mel.shape[0]
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        mel = self.mel[index].astype(np.float32)
+        mel = np.reshape(mel, (1, mel.shape[0], mel.shape[1]))
+        mel = torch.from_numpy(mel)
+        tag = torch.from_numpy(self.tag[index].astype(np.float32))
+        sample = {"mel": mel, "tag": tag}
+
+        return sample
+
+
+# define the data loaders
+def torch_dataset_loader(dataset, batch_size, shuffle, kwargs):
+    """
+    take the h5py dataset
+    """
+    loader = DataLoader(TorchData(dataset),
+                        batch_size=batch_size,
+                        shuffle=shuffle,
+                        **kwargs)
+    return loader
+
+
+train_loader = torch_dataset_loader(Para.TRAIN_DATA_PATH, Para.batch_size, True, Para.kwargs)
+validation_loader = torch_dataset_loader(Para.VAL_DATA_PATH, Para.batch_size, False, Para.kwargs)
+test_loader = torch_dataset_loader(Para.TEST_DATA_PATH, Para.batch_size, False, Para.kwargs)
+
+
+if __name__ == '__main__':
+
+    for index, data_item in enumerate(train_loader):
+        print(data_item['mel'].shape)
+        print(data_item['tag'].shape)
+        break
