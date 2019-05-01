4ce06c8f1f66ec469f9c135824e9fd569d3d530b
Chenyu Xi <xi_chenyu@163.com>
2019-05-01 00:45:32 -0400

    add music dealer

--- scripts/music_dealer.py
+++ scripts/music_dealer.py
@@ -5,17 +5,20 @@ from Paras import Para
 
 
 class MusicDealer:
-    def __init__(self, weight_path, model):
+    def __init__(self, weight_path, model, size=128):
         self.model = model
+        self.index = size
+        self.remove = 15  # remove the first and last 15 s
         model.load_state_dict(torch.load(weight_path, map_location='cpu'))
 
-    @staticmethod
-    def audio_loader(audio_path):
+    def audio_loader(self, audio_path):
         y, _ = librosa.load(audio_path, mono=True, sr=Para.sample_rate)
-        y = librosa.feature.melspectrogram(y=y, sr=Para.sample_rate, n_mels=128).T
+        y = y[self.remove*Para.sample_rate: -1*self.remove*Para.sample_rate]
+        y = librosa.feature.melspectrogram(y=y, sr=Para.sample_rate, n_mels=self.index).T
         y = librosa.power_to_db(y)
-        y = y[:-1 * (y.shape[0] % 128)]
-        chunk_num = int(y.shape[0] / 128)
+        if not y.shape[0] % self.index == 0:
+            y = y[:-1 * (y.shape[0] % self.index)]
+        chunk_num = int(y.shape[0] / self.index)
         mel_chunks = np.split(y, chunk_num)
         return mel_chunks
 
@@ -27,7 +30,7 @@ class MusicDealer:
             tag_list[idx] = 0
 
         for i, data in enumerate(self.audio_loader(audio_path)):
-            data = torch.FloatTensor(data).view(1, 1, 128, 128)  # resize to fit it in model
+            data = torch.FloatTensor(data).view(1, 1, self.index, self.index)  # resize to fit it in model
             predict = self.model(data)
             score, tag = predict.max(1)
             tag_list[int(tag)] = tag_list.get(int(tag)) + float(score)
@@ -35,9 +38,12 @@ class MusicDealer:
         _sum = sum([tag_list.get(key) for key in tag_list])
 
         idx_list.sort(key=lambda x: -1 * tag_list.get(x))
+        tmp = dict()
         for i in idx_list:
             current_genre = Para.dictionary.get(i)
             current_score = tag_list.get(i) / _sum * 100
+            tmp[i] = current_score
             if current_score == 0:
                 break
             print('Genre {0}: {1}%'.format(current_genre, round(current_score, 2)))
+        return idx_list[0], idx_list[1], idx_list[2], tmp
